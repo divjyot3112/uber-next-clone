3d6ed820808f2a1b7ca51d5384ba9f7dd0976636
Divjyot <divjyotskhanuja@gmail.com>
2022-01-13 00:41:35 -0800

    Reverted postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,6 @@
-const purgecss = [
-  "@fullhuman/postcss-purgecss",
-  {
-    content: ["./components/**/*.js", "./pages/**/*.js"],
-    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-  },
-];
-// ---------------------this
 module.exports = {
-  plugins: [
-    "postcss-import",
-    "tailwindcss",
-    "autoprefixer",
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
-  ],
-};
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
